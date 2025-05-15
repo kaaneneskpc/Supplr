dfa4452a1f9f2ad8754b032ce906c54c733048a2
kkapici <kaaneneskpc1@gmail.com>
2025-05-15 21:54:49 +0300

    Added fonts colors and resources

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.androidLibrary)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
 }
@@ -16,21 +16,21 @@ kotlin {
             jvmTarget.set(JvmTarget.JVM_11)
         }
     }
-    
+
     listOf(
         iosX64(),
         iosArm64(),
         iosSimulatorArm64()
     ).forEach { iosTarget ->
         iosTarget.binaries.framework {
-            baseName = "ComposeApp"
+            baseName = "Shared"
             isStatic = true
             //export(libs.kmp.notifier)
         }
     }
-    
+
     sourceSets {
-        
+
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
@@ -48,7 +48,7 @@ kotlin {
             implementation(libs.androidx.lifecycle.runtime.compose)
             // implementation(libs.auth.kmp)
 
-            //implementation(libs.koin.compose)
+            // implementation(libs.koin.compose)
 
             //api(libs.kmp.notifier)
         }
@@ -56,33 +56,18 @@ kotlin {
 }
 
 android {
-    namespace = "com.kaaneneskpc.supplr"
+    namespace = "com.kaaneneskpc.supplr.shared"
     compileSdk = libs.versions.android.compileSdk.get().toInt()
 
     defaultConfig {
-        applicationId = "com.kaaneneskpc.supplr"
         minSdk = libs.versions.android.minSdk.get().toInt()
-        targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-}
-
-dependencies {
-    debugImplementation(compose.uiTooling)
+    lint {
+        targetSdk = libs.versions.android.targetSdk.get().toInt()
+    }
 }
 
---
+++ shared/src/commonMain/kotlin/com/kaaneneskpc/supplr/shared/fonts/Resources.kt
@@ -0,0 +1,74 @@
+package com.kaaneneskpc.supplr.shared.fonts
+
+import supplr.shared.generated.resources.Res
+import supplr.shared.generated.resources.back_arrow
+import supplr.shared.generated.resources.book
+import supplr.shared.generated.resources.cat
+import supplr.shared.generated.resources.check
+import supplr.shared.generated.resources.checkmark_image
+import supplr.shared.generated.resources.close
+import supplr.shared.generated.resources.delete
+import supplr.shared.generated.resources.dollar
+import supplr.shared.generated.resources.edit
+import supplr.shared.generated.resources.google_logo
+import supplr.shared.generated.resources.grid
+import supplr.shared.generated.resources.home
+import supplr.shared.generated.resources.india
+import supplr.shared.generated.resources.log_in
+import supplr.shared.generated.resources.log_out
+import supplr.shared.generated.resources.map_pin
+import supplr.shared.generated.resources.menu
+import supplr.shared.generated.resources.minus
+import supplr.shared.generated.resources.paypal_logo
+import supplr.shared.generated.resources.plus
+import supplr.shared.generated.resources.right_arrow
+import supplr.shared.generated.resources.search
+import supplr.shared.generated.resources.serbia
+import supplr.shared.generated.resources.shopping_cart
+import supplr.shared.generated.resources.shopping_cart_image
+import supplr.shared.generated.resources.unlock
+import supplr.shared.generated.resources.usa
+import supplr.shared.generated.resources.user
+import supplr.shared.generated.resources.vertical_menu
+import supplr.shared.generated.resources.warning
+import supplr.shared.generated.resources.weight
+
+object Resources {
+    object Icon {
+        val Plus = Res.drawable.plus
+        val Minus = Res.drawable.minus
+        val SignIn = Res.drawable.log_in
+        val SignOut = Res.drawable.log_out
+        val Unlock = Res.drawable.unlock
+        val Search = Res.drawable.search
+        val Person = Res.drawable.user
+        val Checkmark = Res.drawable.check
+        val Edit = Res.drawable.edit
+        val Menu = Res.drawable.menu
+        val BackArrow = Res.drawable.back_arrow
+        val RightArrow = Res.drawable.right_arrow
+        val Home = Res.drawable.home
+        val ShoppingCart = Res.drawable.shopping_cart
+        val Categories = Res.drawable.grid
+        val Dollar = Res.drawable.dollar
+        val MapPin = Res.drawable.map_pin
+        val Close = Res.drawable.close
+        val Book = Res.drawable.book
+        val VerticalMenu = Res.drawable.vertical_menu
+        val Delete = Res.drawable.delete
+        val Warning = Res.drawable.warning
+        val Weight = Res.drawable.weight
+    }
+    object Image {
+        val ShoppingCart = Res.drawable.shopping_cart_image
+        val Checkmark = Res.drawable.checkmark_image
+        val Cat = Res.drawable.cat
+        val GoogleLogo = Res.drawable.google_logo
+        val PaypalLogo = Res.drawable.paypal_logo
+    }
+    object Flag {
+        val India = Res.drawable.india
+        val Usa = Res.drawable.usa
+        val Serbia = Res.drawable.serbia
+    }
+}
