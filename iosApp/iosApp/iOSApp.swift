d5c6fae7f37a4468fac11d504f45912a8c623f2e
kkapici <kaaneneskpc1@gmail.com>
2025-05-21 23:51:25 +0300

    Added Google Sign In for IOS

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,10 +1,45 @@
 import SwiftUI
+import GoogleSignIn
+import Firebase
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+    func application(
+      _ app: UIApplication,
+      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+      FirebaseApp.configure()
+      return true
+    }
+    
+    func application(
+        _application: UIApplication,
+        open url: URL,
+        options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+    ) -> Bool {
+        var handled: Bool
+        handled = GIDSignIn.sharedInstance.handle(url)
+        if handled {
+            return true
+        }
+        return false
+    }
+    
+
+
+}
 
 @main
 struct iOSApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+   var body: some Scene {
+      WindowGroup {
+            ContentView().onOpenURL(perform: { url in
+                GIDSignIn.sharedInstance.handle(url)
+            })
+      }
+   }
 }
+
+
