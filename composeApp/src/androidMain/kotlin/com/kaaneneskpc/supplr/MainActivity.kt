38ca69323aa83f4df4eb40131f75ac4fbdd20741
kkapici <kaaneneskpc1@gmail.com>
2025-07-04 20:31:02 +0300

    -Update System Vars

--- composeApp/src/androidMain/kotlin/com/kaaneneskpc/supplr/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/kaaneneskpc/supplr/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.kaaneneskpc.supplr
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -10,8 +13,17 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT,
+                Color.TRANSPARENT
+            ),
+            navigationBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT,
+                Color.TRANSPARENT
+            )
+        )
         super.onCreate(savedInstanceState)
-
         setContent {
             App()
         }
