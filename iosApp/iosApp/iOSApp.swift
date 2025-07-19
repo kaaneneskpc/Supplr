211d8868a997adc5b588469b2b991d11e76dbacf
kkapici <kaaneneskpc1@gmail.com>
2025-07-19 19:57:15 +0300

    -swift code updated.

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,66 +1,43 @@
 import SwiftUI
 import GoogleSignIn
 import Firebase
-import FirebaseCore
-import FirebaseMessaging
-import ComposeApp
-import shared
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+    func application(
+        _ app: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+
+    func application(
+        _application: UIApplication,
+        open url: URL,
+        options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+    ) -> Bool {
+        var handled: Bool
+        handled = GIDSignIn.sharedInstance.handle(url)
+        if handled {
+            return true
+        }
+        return false
+    }
+
+
+
+}
 
 @main
 struct iOSApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-    
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .ignoresSafeArea()
-                .onOpenURL { url in
-                    print("Received URL in onOpenURL: \(url)")
-                    
-                    if GIDSignIn.sharedInstance.handle(url) { return }
-                    
-                    guard let components = URLComponents(url: url, resolvingAgainstBaseURL: true),
-                          let queryItems = components.queryItems else { return }
-                    
-                    let success = queryItems.first(where: { $0.name == "success" })?.value == "true"
-                    let cancel = queryItems.first(where: { $0.name == "cancel" })?.value == "true"
-                    let token = queryItems.first(where: { $0.name == "token" })?.value
-                    
-                    print(
-                        """
-                            ✅ Success: \(success)
-                            ✅ Cancel: \(cancel)
-                            ✅ Token: \(token ?? "null")
-                        """
-                    )
-                     IntentHandlerHelper().navigateToPaymentCompleted(
-                      isSuccess: success ? KotlinBoolean(true) : nil,
-                      error: cancel ? "Payment canceled." : nil,
-                      token: token
-                    )
-                }
+            ContentView().ignoresSafeArea().onOpenURL(perform: { url in
+                GIDSignIn.sharedInstance.handle(url)
+            })
         }
     }
 }
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
-    ) -> Bool {
-        FirebaseApp.configure()
-        NotifierManager.shared.initialize(configuration: NotificationPlatformConfigurationIos(
-                    showPushNotification: true,
-                    askNotificationPermissionOnStart: true,
-                    notificationSoundName: nil
-                  )
-              )
-        return true
-    }
-    
-    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-           Messaging.messaging().apnsToken = deviceToken
-     }
-}
-
-
